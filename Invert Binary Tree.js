0d82bc9c6012b5d3c918d8a1a1060bd3fdc219bc
jayden <jayden@yahoo-inc.com>
2016-08-14 00:10:28 +0800

    Invert Binary Tree

--- Invert Binary Tree.js
+++ Invert Binary Tree.js
@@ -11,7 +11,15 @@
 /**
  * @param {TreeNode} root
  * @return {TreeNode}
- */
+ */ 
 var invertTree = function(root) {
     
+    if(root !== null){
+        //to prevent overwrite the value, I set the new vars for left & right
+        var left  = root.left!==null  ? root.left  : null,
+            right = root.right!==null ? root.right : null;
+        root.left = invertTree(right);
+        root.right = invertTree(left);
+    }
+    return root;
 };
